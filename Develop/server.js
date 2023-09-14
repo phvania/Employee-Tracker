1af937d33d8b242f82284191c1f7957d9df6fbef
phvania <129224910+phvania@users.noreply.github.com>
2023-09-14 00:09:31 -0400

    readme updated

--- Develop/server.js
+++ Develop/server.js
@@ -2,6 +2,10 @@
 // Import and require mysql2
 const mysql = require('mysql2');
 const inquirer = require("inquirer");
+require('console.table')
+
+//const db = require('./db/connection')
+
 
 // Connect to database
 const db = mysql.createConnection(
@@ -15,7 +19,7 @@ const db = mysql.createConnection(
   }
 );
 
-
+//main questions
 function mainquestions() {
   inquirer
     .prompt([
@@ -31,6 +35,7 @@ function mainquestions() {
           "View All Employees",
           "Add Employee",
           "Update Employee Role",
+          "Remove Employee",
           "Exit"
         ]
       }
@@ -42,7 +47,7 @@ function mainquestions() {
           viewAllDepartments()
           break;
         case "Add Department":
-         // console.log("add Department")
+         
           addDepartment()
           break;
         case  "view All Roles":
@@ -60,8 +65,10 @@ function mainquestions() {
           case "Update Employee Role":
             updateEmployeeRole() 
             break;
-
-
+          case "Remove Employee" :
+            removeEmployee()
+            break;
+          
           default:
             //console.log("no case matched")
             db.exit();
@@ -74,9 +81,14 @@ function mainquestions() {
 mainquestions()
 
 
-//function viewAllDepartments(){
-  //console.log("View all departments")
-
+function viewAllDepartments(){
+  db.query('SELECT * FROM department',  function (err, res) {
+    if (err) throw err;
+  console.log("View all departments")
+  mainquestions()
+  })
+}
+//add new department
   function addDepartment() {
     inquirer
           .prompt({
@@ -90,23 +102,164 @@ mainquestions()
               mainquestions();
             } )
           })
-
-          
-          
-
   }
-  /// do work in here
-  //ask questions show results return to main question
-  mainquestions()
+     
+    //view function
+     function viewAllRoles(){
+      db.query('SELECT * FROM  emp_role',  function (err, res) {
+        if (err) throw err;
+      console.log("View all Roles")
+      mainquestions()
+      })
+    }
+     //add new role
+    function addRole() {
+      db.query('SELECT * FROM departments', (err, departments) => {
+        if (err) { console.log(err) }
+        inquirer.prompt([
+          {
+            type: 'input',
+            name: 'title',
+            message: 'what is the name of your role?:'
+          },
+          {
+            type: 'number',
+            name: 'salary',
+            message: 'What is a salary of  this role:'
+          },
+          {
+            type: 'list',
+            name: 'departmentId',
+            message: 'Department ID:',
+          
+            choices: departments.map(department => ({
+              name: `${department.name}`,
+              value: department.id
+            }))
+          }]).then(function (answers) {
+            db.query('INSERT INTO roles SET ?', {
+              title: answers.title,
+              salary: answers.salary,
+              departmentId: answers.departmentId
+            }, function (err, res) {
+              if (err) throw err;
+              console.table(res)
+              mainquestions()
+            })
+          })
+        })
+      }
+//view employees function
+    function viewAllEmployees(){
+      db.query('SELECT * FROM  employee',  function (err, res) {
+        if (err) throw err;
+      console.log("View all Employees")
+      mainquestions()
+      })
+    }
+    //add new employee
+    function  addEmployee()  {
+      db.query('SELECT * FROM roles', (err, roles) => {
+        if (err) { console.log(err) }
+        inquirer.prompt([
+          {
+            type: 'input',
+            name: 'firstName',
+            message: '  First Name of new  employee:'
+          },
+          {
+            type: 'input',
+            name: 'lastName',
+            message: 'Last Name of new employee:'
+          },
+          {
+            type: 'list',
+            name: 'roleId',
+            message: 'Role ID:',
+            choices: roles.map(role => ({
+              name: `${role.title}`,
+              value: role.id
+            }))
+          }
+        
+      ]
+      
+  ).then(function (answers) {
+    db.query('INSERT INTO employees SET ?', {
+      firstName: answers.firstName,
+      lastName: answers.lastName,
+      roleId: answers.roleId,
+      //managerId null for now
+      managerId: null
+    }, function (err, res) {
+      if (err) throw err;
+      console.table(res)
+      start()
+    })
+  })
+})}
 
+//update employee role function
+function updateEmployeeRole() {
 
+  db.query('SELECT * FROM employees', (err, employees) => {
+    if (err) { console.log(err) }
+    db.query(`SELECT * FROM emp_roles`, (err, roles) => {
+      if (err) { console.log(err) }
+      inquirer.prompt([
+        {
+          type: "list",
+          name: "selectEmployee",
+          message: "Select the employee who's role will be updated",
+          choices: employees.map(employee => ({
+            name: `${employee.firstName} ${employee.lastName} - Role ID:${employee.roleId}`,
+            value: employee.id
+          }))
+        },
+        {
+          type: 'list',
+          name: 'updatedRole',
+          message: 'New Role ID:',
+          choices: roles.map(role => ({
+            name: `${role.title}`,
+            value: role.id
+          })) }
+        ]).then(function (answers) {
+          db.query('UPDATE employees SET ? WHERE ?', [{ roleId: answers.updatedRole }, { id: answers.selectEmployee }], function (err, res) {
+            if (err) throw err
+            console.log('Employee role updated!')
+            mainquestions()
+          })
+        })
+      })
+    })
+  }
 
-
-
-
-
-
-
+//remove employee
+function removeEmployee() {
+  db.query('SELECT * FROM employees', (err, employees) => {
+    if (err) { console.log(err) }
+    inquirer.prompt([
+      {
+        type: "list",
+        name: "removeEmp",
+        message: "Select the employee which will be removed",
+        choices: employees.map(employee => ({
+          name: `${employee.firstName} ${employee.lastName}`,
+          value: employee.id
+        }))
+      }
+    ]).then(function (answer) {
+      db.query(`DELETE FROM employees WHERE id = ${answer.removeEmp}`
+        , function (err, res) {
+          if (err) throw err
+          console.log('Employee removed!')
+          start()
+        })
+    })
+  })
+}
+  
 
 
 //module.exports = db;
--- README.md
+++ README.md
@@ -1,1 +1,70 @@
-# Employee-Tracker
+##   TITLE - Employee-Tracker
+
+  ## Table of contents:
+  - [Description](#description)
+  - [Installation](##installation)
+  - [Technologies/Libraries](## Technologies/library)
+  - [Usage](#usage)
+  - [Contribution](#Contribution)
+  - [Testing](#Testing)
+
+  ## Demo:
+
+
+  
+  
+
+  ##screenshot:
+  ![Alt text](image.png)
+  ![Alt text](image-1.png)
+  ![Alt text](image-2.png)
+
+
+
+
+
+
+## Description:
+An Commond line Application for managing a company's employees using node,inquirer and MySQL.
+
+your schema should contain the following three tables:
+
+* `department`
+
+    * `id`: `INT PRIMARY KEY`
+
+    * `name`: `VARCHAR(30)` to hold department name
+
+* `role`
+
+    * `id`: `INT PRIMARY KEY`
+
+    * `title`: `VARCHAR(30)` to hold role title
+
+    * `salary`: `DECIMAL` to hold role salary
+
+    * `department_id`: `INT` to hold reference to department role belongs to
+
+* `employee`
+
+    * `id`: `INT PRIMARY KEY`
+
+    * `first_name`: `VARCHAR(30)` to hold employee first name
+
+    * `last_name`: `VARCHAR(30)` to hold employee last name
+
+    * `role_id`: `INT` to hold reference to employee role
+
+    * `manager_id`: `INT` to hold reference to another employee that is the manager of the current employee (`null` if the employee has no manager)
+
+## Installation:
+In your terminal type:git clone to your github repository.
+open in VS  to build and run
+for more information, visit github help page.
+
+## Technologies-Libraries:
+Inquirer, MySQL, Node, JavaScript
+
+## Demo:
+
+## screenshot:
